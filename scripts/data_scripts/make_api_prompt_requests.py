db997b7fe5d6abbde0c1cd0351a97bba5fb72626
nilsh <nils.hilgers@rwth-aachen.de>
2023-10-18 14:41:49 +0000

    State update

--- scripts/data_scripts/make_api_prompt_requests.py
+++ scripts/data_scripts/make_api_prompt_requests.py
@@ -1,23 +1,33 @@
+import sys
+sys.path.append("/home/nhilgers/setups/dstc11-track5")
+
+
+# Iterate over the inputs in the dataset
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import requests
 import json
 import os, time
-# import openai
-from llama_eval.prompt_llama_eval import prompt_llama_eval
+import openai
+from scripts.data_scripts.get_prompts import get_prompt
 
+
+responses = []
+num_requests = 4173
+num_threads = 16
+file_name = "rg.review_llama2-70b-chat_zero_shot_val.json"
 # openai.api_key = os.environ["OPENAI_API_KEY"]
 
-from scripts.data_scripts.get_prompts import get_prompt
 
 # Your dataset with lists of inputs
 dataset = "val"
 
 responses = []
 
-prompt_llama_str = "[INST] <<SYS>> You are a helpful assistant who can make bookings and reservations. You are provided with a context and must only use the context to answer the users. The context is related to the provided hotel or restaurant names. Context can be reviews from customers or FAQs. FAQs start after token :F: and each new review starts after token :R:. If the context info is not sufficient or contradictory, inform the user.<</SYS>> {}\n\n## Task\nNow give a concise answer as assistant.[/INST]Assistant:"
+prompt_llama_str = "[INST] <<SYS>> Below is a context with customer reviews and FAQs for hotels and restaurants, paired with a conversation between Assistant and User. Answer the question from User appropriately as Assistant. <</SYS>> {} [/INST] Assistant:"
 
 def prompt_llama(input, url='http://gpu-19.apptek.local:8080/generate'):
     data = {}
-    data["parameters"] = {"max_new_tokens":256, "do_sample": True, "temperature": 0.95, "top_p": 0.95, "top_k": 250, "repetition_penalty": 1.15, "repetition_penalty_sustain": 256, "token_repetition_penalty_decay": 128}
+    data["parameters"] = {  "do_sample": True, "min_new_tokens": 5, "max_new_tokens": 150, "num_beams": 5}
     data["inputs"] = prompt_llama_str.format(input)
 
     headers = {
@@ -29,9 +39,7 @@ def prompt_llama(input, url='http://gpu-19.apptek.local:8080/generate'):
     response_text = json.loads(response_text)["generated_text"]
     return response_text
 
-def prompt_openai(input, prompt, url):
-    # Send a payload chat completion request to the OpenAI API for gpt3.5
-    
+def prompt_openai(input, prompt, url):    
     # Split the input text based on the token :U: but keep the token
     context = input.split("User:")[0]
     # Remove the first element of the list which is the context
@@ -57,8 +65,6 @@ def prompt_openai(input, prompt, url):
         )
     return completion["choices"][0]["message"]["content"]
 
-# Iterate over the inputs in the dataset
-from concurrent.futures import ThreadPoolExecutor, as_completed
 
 def process_request(i):
     input, target = get_prompt(i, False, dataset, max_turns=3, max_n_sent=15)
@@ -71,11 +77,6 @@ def process_request(i):
     print(response)
 
     return {"target": target, "response": response.replace("Assistant: ", ""), "id": i}
-
-responses = []
-num_requests = 4173
-num_threads = 16
-file_name = "rg.review_llama_13b_zero_shot_val.json"
 
 with ThreadPoolExecutor(max_workers=num_threads) as executor:
     futures = [executor.submit(process_request, i) for i in range(num_requests)]
--- simple_tgi_inference.py
+++ simple_tgi_inference.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = "http://gpu-19.apptek.local:8080/generate"
+url = "http://gpu-19.apptek.local:8090/generate"
 headers = {"Content-Type": "application/json"}
 
 def read_json_file(file_path):
@@ -9,7 +9,6 @@ def read_json_file(file_path):
         return json.load(file)
 
 # Read the JSON file as a dictionary
-metric_config = read_json_file("metric_likert_config.json")
 
 context = "## Context\nthe cleveland browns ' last playoff win was in 1995 against the new england patriots and their head coach was bill belichick . this was bill 's first playoff win as head coach and the only playoff win as head coach outside the patriots .\n\n## Conversation\nAssistant: i wonder whether letting women into the game would create interest , get more fans . User: oh , it absolutely would . it would be like ... and i know i am going to butcher the name here , but when manon reheaume played in the nhl . my only concern is that if mishandled it might come off like a gimmick . Assistant: i see your point . long term , will women stay ? true . \n i feel like ... there are enough talented women athletes out there , that a woman kicker or qb is possible , perhaps a wide receiver . User: women can be tough too . would a coach like bill belichick hire a woman if she could help him win the super bowl ? Assistant: this is going to come off sounding bad , but it is n't meant to . i think anyone in the front office would look at it from the perspective of generating revenue and views . the sponsors would love the coverage . User: belichick has done well in the post season , so i think he 's looking at it from a strategic point of view . the sponsors would tie her presence into a bunch of social issues and products. Assistant: yes , i agree . the last playoff win was in 1995 against the patriots ."
 
@@ -18,13 +17,7 @@ data = {
     "inputs": "[INST] <<SYS>> For the following context, you will be given a multi-turn conversation between User and Assistant and a potential response for the next turn. Rate how accurate the conversation is in total. An accurate conversation is factually correct with respect to the context. An inaccurate conversation invents facts, which cannot be cited or derived from the context. Carefully assess the score if some parts of the conversation are correct and other parts are wrong. Do not rate helpfulness or the style of the conversation. <</SYS>> {}\n\n## Task\nFIRST provide a one-sentence explanation of your score. Double-check the context you use to verify information. SECOND, state only the score on a scale of 1 to 5. Follow the template.\n\n## Template\nExplanation: <one-sentence explanation>\nAccurate Score: <1-5>[/INST]Explanation:".format(context),
     "parameters": {
         "max_new_tokens": 256,
-        "do_sample": True,
-        "temperature": 0.95,
-        "top_p": 0.95,
-        "top_k": 250,
-        "repetition_penalty": 1.15,
-        "repetition_penalty_sustain": 256,
-        "token_repetition_penalty_decay": 128,
+        "do_sample": False
     },
 }
 
