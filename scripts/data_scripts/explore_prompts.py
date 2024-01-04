26c8653d6463854f3c1c73a0ca4f40936ced156c
nilsh <nils.hilgers@rwth-aachen.de>
2024-01-04 08:07:21 +0000

    Prompt exploration extended

--- scripts/data_scripts/explore_prompts.py
+++ scripts/data_scripts/explore_prompts.py
@@ -1,4 +1,22 @@
 from scripts.data_scripts.get_prompts import get_prompt
+import json
+
+split = "val"
+
+# print the length of the dataset
+with open(f'data/{split}/labels.json', encoding="utf-8") as f:
+    labels = json.load(f)
+print(f"Length of {split} dataset: {len(labels)}")
+
+# print a list of 10 randomly selected IDs for which the target is True
+import random
+random.seed(42)
+print("15 randomly selected IDs for which the target is True:")
+for i in range(15):
+    id = random.randint(0, len(labels))
+    while not labels[id]['target']:
+        id = random.randint(0, len(labels))
+    print(id)
 
 label_print = False
 print("[If you want to switch label printing, enter `l` instead of an ID.")
@@ -15,5 +33,5 @@ while True:
             continue
         else:
             raise ValueError("Unknown input.")
-    print(get_prompt(id, split="val", dataset="data", label_print=label_print, max_turns=15, max_n_sent=15)[0])
+    print(get_prompt(id, split=split, dataset="data", label_print=label_print, max_turns=15, max_n_sent=15)[0])
     
